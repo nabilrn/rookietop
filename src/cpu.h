adb780424fe015e488eece82d14b8e44fa8a72d6
Nabil <116415990+nabilrn@users.noreply.github.com>
2026-08-16 17:19:12 +0700

    feat: add CPU sampling interface

---
+++ src/cpu.h
@@ -0,0 +1,23 @@
+#ifndef ROOKIETOP_CPU_H
+#define ROOKIETOP_CPU_H
+
+#include <stdint.h>
+
+struct cpu_sample {
+    uint64_t user;
+    uint64_t nice;
+    uint64_t system;
+    uint64_t idle;
+    uint64_t iowait;
+    uint64_t irq;
+    uint64_t softirq;
+    uint64_t steal;
+};
+
+int cpu_parse_line(const char *line, struct cpu_sample *out);
+int cpu_read(struct cpu_sample *out);
+int cpu_usage(const struct cpu_sample *prev,
+              const struct cpu_sample *curr,
+              double *out_percent);
+
+#endif
